ffb9d01095534a17b7a802512bbdf01f33a8c68f
James Dennes <jdennes@gmail.com>
2016-04-30 14:54:45 +0200

    Add test setup

---
+++ test/test_buienalarm_scraper.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require_relative "../lib/buienalarm_scraper"
+
+class TestBuienAlarmScraper < Minitest::Test
+
+  def test_that_scrape_raises_an_error_when_no_data_is_found
+    location = "not-a-valid-location"
+    error = assert_raises RuntimeError do
+      Buienalarm::Scraper.scrape(location)
+    end
+    assert_equal "No projected rainfall data for '#{location}' found",
+      error.message
+  end
+
+end
