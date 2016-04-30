a84dfb2b50fb23528fc2a5fd3bf7a5a7ca3ea004
James Dennes <jdennes@gmail.com>
2016-04-30 15:12:52 +0200

    Add more assertions

--- test/test_buienalarm_scraper.rb
+++ test/test_buienalarm_scraper.rb
@@ -1,6 +1,7 @@
 require "minitest/autorun"
 require_relative "../lib/buienalarm_scraper"
 
+# This doesn't do any HTTP mocking. This tests real scraping functionality.
 class TestBuienalarmScraper < Minitest::Test
 
   def test_that_scrape_raises_an_error_when_no_data_is_found
@@ -16,9 +17,11 @@ class TestBuienalarmScraper < Minitest::Test
     location = "rotterdam"
     result = Buienalarm::Scraper.scrape(location)
     assert result.is_a? Array
-
-    # TODO: Add more assertions
-
+    assert_equal 25, result.size
+    entry = result.first
+    assert entry[:time].is_a? DateTime
+    assert entry[:rainfall].is_a? Float
+    assert entry[:level].is_a? String
   end
 
 end
